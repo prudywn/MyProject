bd341f8902b3092b52ba13999015444f128b499d
itsPrudy <dencecira463@gmail.com>
2024-04-26 11:03:50 +0300

    I've been able to make the footer presentable in small screens using media queries

--- project/build/js/main.js
+++ project/build/js/main.js
@@ -1,16 +1,21 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
+    const footer_Btn = document.getElementById('footer_button')
+    const socialMenu =document.getElementById('socialMenu')
 
     const toggleMenu = () =>{
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
+        socialMenu.classList.toggle('hidden')
+        socialMenu.classList.toggle('flex')
        /*  hamburgerBtn.classList.toggle('hamburger-btn') */
     }
 
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
-    
+    footer_Btn.addEventListener('click',toggleMenu)
+    socialMenu.addEventListener('click', toggleMenu)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
